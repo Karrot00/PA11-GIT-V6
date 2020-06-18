2b2f506a4ace6c1887b82fd270fa0a2bb7e09320
Karen <karenchin_20_11@outlook.com>
2020-06-18 14:44:05 +0800

    Created by Karen

---
+++ 23_PA11_Karen/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    public int score;
+    public Text Txt_Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4f, 4f), transform.position.z);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+
+        if (collision.collider.tag == "Score")
+        {
+            Destroy(collision.gameObject);
+            score++;
+            Txt_Score.text = "Score: " + score;
+        }
+    }
+}
